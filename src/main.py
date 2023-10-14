94486f959acd3d5b5d11ff6e3941851fc534825c
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-10-14 17:01:05 -0400

    Check for pygame.MOUSEBUTTONDOWN instead

--- src/main.py
+++ src/main.py
@@ -56,7 +56,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
             canvas.fill(pos)
 
